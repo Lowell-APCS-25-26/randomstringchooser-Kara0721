1f0d1b8bb5c06c09648d5eeec91c0a1795801b8b
Kara0721 <karactruong@gmail.com>
2026-04-25 04:44:06 +0000

    done

--- lib/src/main/java/org/APCSLowell/RandomStringChooser.java
+++ lib/src/main/java/org/APCSLowell/RandomStringChooser.java
@@ -1,4 +1,19 @@
 package org.APCSLowell;
+import java.util.*;
 
 // FRQ: https://secure-media.collegeboard.org/digitalServices/pdf/ap/ap16_frq_computer_science_a.pdf
-public class RandomStringChooser {}
+public class RandomStringChooser {
+    private ArrayList <String> words;
+    public RandomStringChooser(String[]wordArray){
+        words = new ArrayList<String>();
+        for(int i = 0; i < wordArray.length; i++)
+            words.add(wordArray[i]);
+    }
+    public String getNext(){
+        while(words.size()>0){
+            int k = (int)(Math.random()*words.size());
+            return words.remove(k);
+        }
+        return "NONE";
+    }
+}
--- lib/src/test/java/org/APCSLowell/RandomStringChooserTest.java
+++ lib/src/test/java/org/APCSLowell/RandomStringChooserTest.java
@@ -1,5 +1,5 @@
 package org.APCSLowell;
-
+import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
